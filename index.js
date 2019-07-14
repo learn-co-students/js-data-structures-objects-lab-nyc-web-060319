74bcced2b78333603f8e435d205b40f59de50cc6
TimeSmash <TimeSmash@github.com>
2019-07-13 22:33:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign({}, driver, {[key]: value})
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    Object.assign(driver, {[key] : value})
+	return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+        let deleted_key_driver = Object.assign({}, driver) // Merge {} with driver so driver stays unaltered
+        delete deleted_key_driver[key] //d_k_d is essentially a copy of driver, but we can mess with it without altering driver
+        return deleted_key_driver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+}
+
+
